ec918d4629dd8292c3c49e98b448aa32542bf150
Shing Lyu <shing.lyu@gmail.com>
2015-08-25 21:07:29 +0800

    paragraph indent; clean up push generator code

--- generator.js
+++ generator.js
@@ -44,6 +44,30 @@ function getRandomPushes(n, pushes){
   return selectedPushes
 }
 
+var PushGenerator = function(push){
+  this.push = push;
+  this.templates = [
+    ['網友', '回應'],
+    ['網友', '附和說'],
+    ['網友', '舉例'],
+    ['網友', '則一針見血提出觀點，認為'],
+    ['網友', '也說'],
+    ['網友', '也贊同'],
+    ['網友', '建議'],
+    ['網友', '留言稱'],
+    ['網友', '直言'],
+    //'VOLK11和beryllos異口同聲表示',
+    //['Ptt網友紛紛表示同情'],
+    //['也有不少網友認為'],
+  ]
+}
+
+PushGenerator.prototype.generate = function(){
+    var rand = Math.floor(Math.random() * (this.templates.length));
+    var content = this.push.content.substr(1) // first char is ":"
+    return this.templates[rand][0] + " " + this.push.user + " " + this.templates[rand][1] + "：「" + content + "」";
+}
+
 var parse = function(html){
   var doc = document.implementation.createHTMLDocument("example");
   doc.documentElement.innerHTML = html;
@@ -78,21 +102,23 @@ var parse = function(html){
 
   template = ""
   
-  template += "<h2>網路瘋傳：「 " + title+ "」，網友都驚呆了！</h2>"
+  template += "<h2>網路瘋傳 " + title+ "，網友都驚呆了！</h2>"
   template += "<footnote>本報訊 | " + (new Date()).toLocaleString() + "</footnote><br/><br/>"
-  template += "網友 " + author + " 在PTT上PO文表示:「" + allContent.replace(/\n\s*/g, ",").trim() + "」,引起網友熱議。\n\n"
+  template += "<p>網友 " + author + " 在PTT上PO文表示:「" + allContent.replace(/--.*--/,'').replace(/\n\s*/g, ",").trim() + "」，引起網友熱議。</p>"
   var pushCount = 5
-  var selectedComments = getRandomPushes(5, comments)
-  for (var i = 0; i < selectedComments.length; i++) {
-    template += "網友 " + selectedComments[i]['user'] + " 認為" + selectedComments[i]['content'] + ", "
-  }
+  
+  for (var n = 0; n < 2; n++) {
+    var selectedComments = getRandomPushes(4, comments)
+    template += "<p>"
+    for (var i = 0; i < selectedComments.length; i++) {
+      var pgen = new PushGenerator(selectedComments[i])
+      //template += "網友 " + selectedComments[i]['user'] + " 認為" + selectedComments[i]['content'] + ", "
+      template += pgen.generate() + "，"
+    }
+    template += pgen.generate() + "。"
 
-  template += "\n\n"
-  var selectedComments = getRandomPushes(5, comments)
-  for (var i = 0; i < selectedComments.length; i++) {
-    template += "網友 " + selectedComments[i]['user'] + " 認為" + selectedComments[i]['content'] + ", "
+    template += "</p>"
   }
-  template += "<br/><br/>"
   template += "原文出處： <a href='" + originalLink + "'>" + originalLink + "</a>";
   console.log(template)
   document.getElementById("main_article").innerHTML = template.replace(/\n/g, "<br>", "g")
