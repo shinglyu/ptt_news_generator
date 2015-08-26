52619ec2e4686a13a3a140fbc481a3838c3ec170
Shing Lyu <shing.lyu@gmail.com>
2015-08-26 23:34:26 +0800

    Added disp cc support

--- generator.js
+++ generator.js
@@ -23,11 +23,26 @@ document.getElementById('file').addEventListener('change', function(e){
   reader.readAsText(e.target.files[0]);
 });
 
+document.getElementById('submit-disp').addEventListener('click', function(e){
+  //console.log('hi')
+  var url = document.getElementById('text').value;
+  $.ajax('http://crossorigin.me/'+ url).done(function(res){
+      display(parseDispCc(res));
+  }).fail(function(res){
+    alert('無效網址，請檢查網址是否正確')
+  })
+  document.getElementById("main_article").innerHTML = '<h1>Loading...</h1>';
+
+});
+
 //}
   //
 
 function getRandomPushes(n, pushes){
   var indexes = []
+  if (n >= pushes.length) {
+    return Array.apply(null, Array(pushes.length)).map(function (_, i) {return i;});
+  }
   while (indexes.length < n) {
     var rand = Math.floor(Math.random() * (pushes.length));
     if (indexes.indexOf(rand) < 0){
@@ -69,16 +84,17 @@ PushGenerator.prototype.generate = function(){
 }
 
 var parse = function(html){
+  var pttArticle = {}
   var doc = document.implementation.createHTMLDocument("example");
   doc.documentElement.innerHTML = html;
   var metas = doc.getElementsByClassName('article-meta-value');
-  var author = metas[0].textContent;
-  var board = metas[1].innerHTML;
-  var title = metas[2].innerHTML;
-  var timePublished = metas[3].innerHTML;
-  var originalLink= doc.getElementsByClassName('fb-like')[0].dataset['href'];
+  pttArticle['author'] = metas[0].textContent;
+  pttArticle['board'] = metas[1].innerHTML;
+  pttArticle['title'] = metas[2].innerHTML;
+  pttArticle['timePublished'] = metas[3].innerHTML;
+  pttArticle['originalLink']= doc.getElementsByClassName('fb-like')[0].dataset['href'];
   //var allContent = doc.getElementById('main-content').cloneNode(false).textContent;
-  var allContent = $('#main-content',doc).clone().children().remove().end().text();
+  pttArticle['allContent']= $('#main-content',doc).clone().children().remove().end().text();
   //console.log(doc.getElementById('main-content').childNodes)
   //var allContent = doc.getElementById('main-content').textContent
   var commentsDom = doc.getElementsByClassName('push')
@@ -91,6 +107,41 @@ var parse = function(html){
     comment['time'] = commentsDom[i].getElementsByClassName('push-ipdatetime')[0].textContent;
     comments.push(comment);
   }
+  pttArticle['comments'] = comments;
+  return pttArticle
+}
+
+var parseDispCc = function(html){
+  var pttArticle = {}
+  var doc = document.implementation.createHTMLDocument("example");
+  doc.documentElement.innerHTML = html;
+  var metas = doc.getElementsByClassName('TH_div')[0].getElementsByTagName('span');
+  pttArticle['author'] = metas[4].textContent;
+  pttArticle['board'] = metas[1].innerHTML;
+  pttArticle['title'] = metas[6].textContent;
+  pttArticle['timePublished'] = metas[8].innerHTML;
+  var records = doc.getElementsByClassName('record')
+  //FIXME: link is not working
+  //pttArticle['originalLink']= records[records.length -2].textContent.substr(7);
+  pttArticle['originalLink']= "http://disp.cc"
+  //var allContent = doc.getElementById('main-content').cloneNode(false).textContent;
+  pttArticle['allContent']= $($('#text', doc).children()[0]).clone().children().remove().end().text()
+  //console.log(doc.getElementById('main-content').childNodes)
+  //var allContent = doc.getElementById('main-content').textContent
+  var commentsDom = doc.getElementsByClassName('push_row')
+  var comments = []
+  for (var i = 0; i < commentsDom.length; i++) {
+    var comment = {}
+    comment['type'] = commentsDom[i].getElementsByTagName('span')[0].textContent.substr(0,1);
+    comment['user'] = commentsDom[i].getElementsByTagName('span')[0].textContent.substr(2);
+    comment['content'] = commentsDom[i].getElementsByTagName('span')[2].textContent;
+    comment['time'] = commentsDom[i].getElementsByClassName('push-right')[0].textContent;
+    comments.push(comment);
+  }
+  pttArticle['comments'] = comments;
+  console.log(pttArticle)
+  return pttArticle
+}
   /*
   console.log(author)
   console.log(board)
@@ -100,27 +151,41 @@ var parse = function(html){
   console.log(comments)
   */
 
+function display(pttArticle){
   template = "";
   
-  template += "<h2>網路瘋傳 " + title+ "，網友都驚呆了！</h2>";
+  template += "<h2>網路瘋傳 " + pttArticle.title+ "，網友都驚呆了！</h2>";
   template += "<footnote>本報訊 | " + (new Date()).toLocaleString() + "</footnote><br/><br/>";
-  template += "<p>網友 " + author + " 在PTT上PO文表示:「" + allContent.replace(/--.*--/,'').replace(/\n\s*/g, ",").trim() + "」，引起網友熱議。</p>";
+  template += "<p>網友 " + pttArticle.author + " 在PTT上PO文表示:「" + pttArticle.allContent.replace(/--.*--/,'').replace(/\n\s*/g, ",").trim() + "」，引起網友熱議。</p>";
   var pushCount = 5;
-  
-  for (var n = 0; n < 2; n++) {
-    var selectedComments = getRandomPushes(5, comments);
-    template += "<p>";
-    for (var i = 0; i < selectedComments.length - 1; i++) {
-      var pgen = new PushGenerator(selectedComments[i]);
-      //template += "網友 " + selectedComments[i]['user'] + " 認為" + selectedComments[i]['content'] + ", "
-      template += pgen.generate() + "，";
-    }
-    var pgen = new PushGenerator(selectedComments[selectedComments.length - 1]);
-    template += pgen.generate() + "。";
 
-    template += "</p>";
+  if (pttArticle.comments.length <= pushCount) {
+      for (var i = 0; i < pttArticle.comments.length - 1; i++) {
+        var pgen = new PushGenerator(pttArticle.comments[i]);
+        //template += "網友 " + selectedComments[i]['user'] + " 認為" + selectedComments[i]['content'] + ", "
+        template += pgen.generate() + "，";
+      }
+      if (pttArticle.comments.length > 0){
+        var pgen = new PushGenerator(pttArticle.comments[pttArticle.comments.length - 1]);
+        template += pgen.generate() + "。";
+      }
   }
-  template += "原文出處： <a href='" + originalLink + "'>" + originalLink + "</a>";
+  else {
+    for (var n = 0; n < 2; n++) {
+      var selectedComments = getRandomPushes(5, pttArticle.comments);
+      template += "<p>";
+      for (var i = 0; i < selectedComments.length - 1; i++) {
+        var pgen = new PushGenerator(selectedComments[i]);
+        //template += "網友 " + selectedComments[i]['user'] + " 認為" + selectedComments[i]['content'] + ", "
+        template += pgen.generate() + "，";
+      }
+      var pgen = new PushGenerator(selectedComments[selectedComments.length - 1]);
+      template += pgen.generate() + "。";
+
+      template += "</p>";
+    }
+  }
+  template += "原文出處： <a href='" + pttArticle.originalLink + "'>" + pttArticle.originalLink + "</a>";
   //console.log(template)
   document.getElementById("main_article").innerHTML = template.replace(/\n/g, "<br>", "g");
 }
@@ -128,4 +193,7 @@ var parse = function(html){
 document.getElementById('example-0').onclick = function(){ parse(examples[0]); }; 
 document.getElementById('example-1').onclick = function(){ parse(examples[1]); }; 
 
-parse(examples[0]);
+display(parse(examples[0]));
+
+  
+
